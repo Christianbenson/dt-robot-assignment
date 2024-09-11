4a5564cb5967245f515c14de3d9517a18bfa7840
Christian Benson <christianhabenson@gmail.com>
2024-09-11 14:48:00 +0200

    Added room class, which keeps track of the size of the room

---
+++ app/src/main/java/org/example/Room.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Room {
+    private int width;
+    private int depth;
+
+    public Room(int width, int depth) {
+        this.width = width;
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
