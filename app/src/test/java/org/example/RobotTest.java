10c6454d82484ec5596e0a78df8cc43a90ffa3f8
Christian Benson <christianhabenson@gmail.com>
2024-09-11 15:03:26 +0200

    Added empty methods for robot tests

---
+++ app/src/test/java/org/example/RobotTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotTest {
+    @Test void robotMovesForward() {
+
+    }
+
+    @Test void robotTurnsLeft() {
+
+    }
+
+    @Test void robotTurnsRight() {
+
+    }
+
+    @Test void robotTurns360Degrees() {
+
+    }
+
+    @Test void robotThrowsExceptionOutOfBounds() {
+        
+    }
+}
