f850da6c54083a654586fd3b762c00268a037464
Christian Benson <christianhabenson@gmail.com>
2024-09-11 15:11:31 +0200

    Change direction string to char, add room attribute on robot so it knows which room its in

--- app/src/main/java/org/example/Robot.java
+++ app/src/main/java/org/example/Robot.java
@@ -3,13 +3,15 @@ package org.example;
 public class Robot {
     private int xCoord;
     private int yCoord;
-    private String direction;
+    private char direction;
+    private Room currentRoom;
 
     //TODO implement input validation, no negative coords + only N,E,W,S as direction
-    public Robot(int xStart, int yStart, String directionStart) {
+    public Robot(int xStart, int yStart, char directionStart, Room roomStart) {
         xCoord = xStart;
         yCoord = yStart;
         direction = directionStart;
+        currentRoom = roomStart;
     }
 
     public void leftTurn() {
@@ -32,7 +34,7 @@ public class Robot {
         return yCoord;
     }
 
-    public String getDirection() {
+    public char getDirection() {
         return direction;
     }
 }
